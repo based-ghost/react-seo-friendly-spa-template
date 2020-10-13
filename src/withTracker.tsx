680fe10c755df7a72b36e24e2f24d1ddff94c65a
Matt Areddia <mareddia@protonmail.com>
2020-10-13 18:34:20 -0500

    add cookieFlags property to react.ga options

--- src/withTracker.tsx
+++ src/withTracker.tsx
@@ -3,7 +3,11 @@ import ReactGA, { FieldsObject } from 'react-ga';
 import { RouteComponentProps } from 'react-router-dom';
 
 // Initialize the react-ga plugin using your issued GA tracker code
-ReactGA.initialize('UA-0000000-0');
+ReactGA.initialize('UA-0000000-0', {
+  gaOptions: {
+    cookieFlags: 'max-age=7200;secure;samesite=none'
+  }
+});
 
 // React.FC component used as a wrapper for route components - e.g. withTracker(RouteComponent)
 export const withTracker = <P extends RouteComponentProps>(
